7f5b0c09d29a8761a06fbac27b903c4d7bd23806
Ingrid Moreira <168448044+Quenze@users.noreply.github.com>
2024-07-25 00:58:09 +0000

    Alteração

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,5 +1,4 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
-
 info1 = input("Digite a primeira informação")
 info2 = input("Digite a segunda informação")
 
