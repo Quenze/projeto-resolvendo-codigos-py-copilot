2d125e7a8eba41f079f4bb88dd52ae0449fd4a20
Ingrid Moreira <168448044+Quenze@users.noreply.github.com>
2024-07-25 02:06:29 +0000

    Completo

--- ope_mat.py
+++ ope_mat.py
@@ -1,1 +1,19 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite um número inteiro: "))
+
+operacao = input("Digete a operação que deseja realizar (+, -, *, /): ")
+
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-':
+    print(abs(num1 - num2))
+elif operacao == '*':
+    print(num1 * num2)
+elif operacao == '/':
+    print(num1 / num2)
+else:
+    print("Operação inválida")
+
+
